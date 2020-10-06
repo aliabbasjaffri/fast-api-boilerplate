09ca0561887f8e1214fa9d7969b37a77b6cf39e3
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-10-06 17:09:50 +0200

    Commented user and group association in gunicorn weil some rights issue

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,14 +4,14 @@ from pathlib import Path
 
 Path("/var/log/gunicorn/").mkdir(parents=True, exist_ok=True)
 
-
-pidfile = '/var/run/gunicorn.pid'
 errorlog = '/var/log/gunicorn/gunicorn.log'
 loglevel = 'warning'
 bind = 'unix:/opt/api/app.sock'
+# Daemon turned off because of the note on the URL
+# https://docs.gunicorn.org/en/latest/deploy.html#monitoring
 daemon = False
 workers = cpu_count() * 2 + 1
 threads = 2
-worker_class = '/usr/local/bin/uvicorn.workers.UvicornWorker'
-user = 'www-data'
-group = 'www-data'
+worker_class = 'uvicorn.workers.UvicornWorker'
+# user = 'www-data'
+# group = 'www-data'
